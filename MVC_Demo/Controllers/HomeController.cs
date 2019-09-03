af6cf999106619e146ca241b95d0af35627a672e
nicke99 <nick@deltafamily.co.uk>
2019-09-03 20:20:55 +0100

    updated index page

--- MVC_Demo/Controllers/HomeController.cs
+++ MVC_Demo/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MVC_Demo.Models;
 
@@ -28,6 +24,7 @@ namespace MVC_Demo.Controllers
 
             return View();
         }
+
 
         public IActionResult Privacy()
         {
